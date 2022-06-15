081462a1b52faac90d714885516df5fda7ba30cf
Thien Go <58649187+goriummaximum@users.noreply.github.com>
2022-06-16 00:24:40 +0700

    Update gateway.py

--- lab3/gateway/gateway.py
+++ lab3/gateway/gateway.py
@@ -1,17 +1,68 @@
 print("IoT Gateway")
+from gateway.gateway import sendSerial
 import paho.mqtt.client as mqttclient
 import time
 import json
 import serial.tools.list_ports
 
+'''
+software timer
+'''
+TIMER_CYCLE = 10 #ms
+
+timer_counter = 0
+timer_flag = 0
+
+def software_timer_init():
+    global timer_counter, timer_flag
+    timer_counter = 0
+    timer_flag = 0
+
+def get_timer_flag():
+    return timer_flag
+
+def set_timer(duration):
+    global timer_counter, timer_flag
+    if (timer_counter == 0):
+        timer_flag = 0
+        timer_counter = duration / TIMER_CYCLE
+
+def run_timer():
+    global timer_counter, timer_flag
+    if (timer_counter > 0):
+        timer_counter = timer_counter - 1
+        if (timer_counter == 0):
+            timer_flag = 1
+
+'''
+FSM
+'''
+IDLE = 0
+SEND_DATA = 1
+WAIT_ACK = 2
+SEND_ACK = 3
+ERROR_LOG = 4
+
+state = IDLE
+SEND_MAX = 5
+SEND_INTERVAL = 100 #ms
+counter_failure = 0
+
+serial_data_available = 0
+mqtt_data_available = 0
+ack_received_successful = 0
+
+'''
+MQTT and SERIAL
+'''
 BROKER_ADDRESS = "demo.thingsboard.io"
 PORT = 1883
 mess = ""
 
-#TODO: Add your token and your comport
-#Please check the comport in the device manager
+cmd = 1
+
 THINGS_BOARD_ACCESS_TOKEN = "VJrNYpE2cgnjKpsIzRl4"
-bbc_port = "COM11"
+bbc_port = "/dev/ttyACM0"
 if len(bbc_port) > 0:
     ser = serial.Serial(port=bbc_port, baudrate=115200)
 
@@ -22,49 +73,60 @@ def processData(data):
     print(splitData)
     serialized_data = {}
     serialized_data[splitData[1]] = splitData[2]
-    client.publish('v1/devices/me/telemetry', json.dumps(serialized_data), 1)
+    return serialized_data
 
-def readSerial():
+def read_serial():
     bytesToRead = ser.inWaiting()
     if (bytesToRead > 0):
-        global mess
+        global mess, serial_data_available, ack_received_successful
         mess = mess + ser.read(bytesToRead).decode("UTF-8")
-        while ("#" in mess) and ("!" in mess):
+        if ("#" in mess) and ("!" in mess):
             start = mess.find("!")
             end = mess.find("#")
-            processData(mess[start:end + 1])
+            serialized_data = processData(mess[start:end + 1])
+            if (serialized_data["ACK"] == "1"):
+                ack_received_successful = 1
+            else:
+                client.publish('v1/devices/me/telemetry', json.dumps(serialized_data), 1)
+                serial_data_available = 1
+
             if (end == len(mess)):
                 mess = ""
             else:
                 mess = mess[end+1:]
 
+def send_serial(data):
+    if len(bbc_port) <= 0:
+        return 0
+
+    ser.write((data + "#").encode())
+    return 1
 
 def subscribed(client, userdata, mid, granted_qos):
     print("Subscribed...")
 
 def recv_message(client, userdata, message):
+    global mqtt_data_available, cmd
     print("Received: ", message.payload.decode("utf-8"))
-    #temp_data = {'value': True}
-    cmd = 1
+    temp_data = {'value': True}
 
     try:
         jsonobj = json.loads(message.payload)
         if jsonobj['method'] == "setLED":
             #set cmd for Microblit platform
-            cmd = 0 if (jsonobj['params'] == True) else 1
+            cmd = 1 if (jsonobj['params'] == True) else 0
             #send feedback to update button in Thingsboard via attribute getLED
             temp_data['getLED'] = jsonobj['params']
             client.publish('v1/devices/me/attributes', json.dumps(temp_data), 1)
 
         if jsonobj['method'] == "setFAN":
-            cmd = 2 if (jsonobj['params'] == True) else 3
+            cmd = 3 if (jsonobj['params'] == True) else 2
             temp_data['getFAN'] = jsonobj['params']
             client.publish('v1/devices/me/attributes', json.dumps(temp_data), 1)
+
+        mqtt_data_available = 1
     except:
         pass
-
-    if len(bbc_port) > 0:
-        ser.write((str(cmd) + "#").encode())
 
 def connected(client, usedata, flags, rc):
     if rc == 0:
@@ -84,10 +146,43 @@ client.loop_start()
 client.on_subscribe = subscribed
 client.on_message = recv_message
 
-
+#run Stop and Wait FSM
+software_timer_init()
 while True:
+    if (state == IDLE):
+        read_serial()
+        if (serial_data_available == 1):
+            serial_data_available = 0
+            state = SEND_ACK
+        elif (mqtt_data_available == 1):
+            mqtt_data_available = 0
+            state = SEND_DATA
 
-    if len(bbc_port) > 0:
-        readSerial()
+    elif (state == SEND_DATA):
+        if (send_serial(cmd) == 1):
+            set_timer(SEND_INTERVAL)
+            state = WAIT_ACK
 
-    time.sleep(1)
+    elif (state == WAIT_ACK):
+        read_serial()
+        if (ack_received_successful == 1):
+            state = IDLE
+        else:
+            if (get_timer_flag() == 1):
+                counter_failure = counter_failure + 1
+                if (counter_failure >= SEND_MAX):
+                    counter_failure = 0
+                    state = ERROR_LOG
+                else:
+                    state = SEND_DATA
+
+    elif (state == SEND_ACK):
+        if (send_serial("ACK") == 1):
+            state = IDLE
+
+    elif (state == ERROR_LOG):
+       print("send failed")
+       state = IDLE
+
+    run_timer()
+    time.sleep(TIMER_CYCLE / 1000)
